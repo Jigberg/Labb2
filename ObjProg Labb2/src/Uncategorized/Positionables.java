2a9471b68dc8a39aca0624a7e960b8fb2f9c1b2a
Fredins <fredin.martin@gmail.com>
2020-11-26 16:20:27 +0100

    Added Movables, Postionables...
    Added Movables, Direction, Postionables, Speed.
    Moved a little code but didn't fix the errors.

---
+++ ObjProg Labb2/src/Uncategorized/Positionables.java
@@ -0,0 +1,19 @@
+package Uncategorized;
+
+public class Positionables{
+    private double x; // The X position
+    private double y; // The Y position
+    private Direction direction;
+
+    public Positionables(double x, double y, Direction direction){
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+    public double getx() { return x; }
+    public double gety() { return y; }
+    public void setx(double x) { this.x = x; }
+    public void sety(double y) { this.y = y; }
+    public Direction getDirection() {return direction; }
+    public void setDirection(Direction direction){ this.direction = direction; }
+}
--- ObjProg Labb2/src/main.java
+++ ObjProg Labb2/src/main.java
@@ -1,7 +1,3 @@
-public class main {
-
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
+public class main { public static void main(String[] args) { System.out.println("hello world"); }
 
 }
